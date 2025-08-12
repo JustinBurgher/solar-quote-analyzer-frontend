339b1451356708c8e70b67cbdad14da2eadfe51f
JustinBurgher <Sellingorconsuming@gmail.com>
2025-08-12 18:53:00 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Upgrade from './pages/Upgrade.jsx';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate, useLocation } from 'react-router-dom';
 
